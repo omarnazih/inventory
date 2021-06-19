7e6412896dcad4200734e0c1dc49fe421857dbb4
omarnazih <omarnazeh295@gmail.com>
2021-06-19 18:09:22 +0200

    Added 5 more components [Button, Task, Tasks, About, Footer]

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,25 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text, onclick }) => {
-    return <button
-        style={{ backgroundColor: color }}
-        className='btn' onClick={onclick}>
-        {text}
-        </button>
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className='btn'
+    >
+      {text}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  color: 'steelblue',
 }
 
 Button.propTypes = {
-    color: PropTypes.string,
-    text: PropTypes.string,
-    onclick: PropTypes.func,
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 }
+
 export default Button
